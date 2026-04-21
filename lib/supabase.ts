ae5c0966d90def665f0c9dad69ba125335a8570c
probmoonshards <120243109+probmoonshards@users.noreply.github.com>
2026-04-21 18:29:01 +0400

    Delete lib directory

--- lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://cumaqxnrmxjghdwznouj.supabase.co'
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1bWFxeG5ybXhqZ2hkd3pub3VqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1NzQzMzIsImV4cCI6MjA5MjE1MDMzMn0.ahLv7Ps_EZfKAyIjOhM-8Gmt_nWAqD0TCfXJdMA14Fc'
-
-export const sb = createClient(SUPABASE_URL, SUPABASE_ANON)
